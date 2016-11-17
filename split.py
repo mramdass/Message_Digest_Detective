7b21e86255dde0b0f60f2f9c97cfb02ef4417d24
Munieshwar Ramdass <mramdass@nyu.edu>
2016-11-17 13:17:47 -0500

    Update split.py

--- split.py
+++ split.py
@@ -5,13 +5,17 @@
     26 October 2016
 
     Final Project - Message Digest Detective
+
+    Functions presented here are all threaded and works
+    best if hash lists are on separate drives. This scipt
+    is meant to be run only once during the set up phase.
 '''
 
 try:
     import os, sys, threading, tempfile, json
     from itertools import chain, islice, izip_longest
     from threading import Thread
-    from zipfile import ZipFile
+    from zipfile import ZipFile, ZIP_DEFLATED
 except Exception as e:
     print '\t', e
 
@@ -44,12 +48,11 @@ def unzip_metadata(zipped, out):
                 c += 1
             metadata[out][c - 1] = metadata[out][current]
             del metadata[out][current]
-# ________________________________________________________________
+# _______________________________________________________________________________
+# The following two functions are referenced and altered from the following link:
 # http://stackoverflow.com/questions/16289859/splitting-large-text-file-into-smaller-text-files-by-line-numbers-using-python
 
 def grouper(n, iterable, fillvalue=None):
-    "Collect data into fixed-length chunks or blocks"
-    # grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx
     args = [iter(iterable)] * n
     return izip_longest(fillvalue=fillvalue, *args)
 
@@ -65,30 +68,38 @@ def split_files(zipped, out_dir):
                             break
                         fout.write(line.rstrip() + '\n')
                 os.rename(fout.name, out_dir + 'RDS_{0}.txt'.format(i * n + j))
-# ________________________________________________________________
+# _______________________________________________________________________________
 
-'''
-t1 = Thread(target=split_files, args=('RDS_Split/A/RDS_253_A.zip', 'RDS_Split/A/'))
-t2 = Thread(target=split_files, args=('RDS_Split/B/RDS_253_B.zip', 'RDS_Split/B/'))
-t3 = Thread(target=split_files, args=('RDS_Split/C/RDS_253_C.zip', 'RDS_Split/C/'))
-t4 = Thread(target=split_files, args=('RDS_Split/D/RDS_253_D.zip', 'RDS_Split/D/'))
-for thread in [t1, t2, t3, t4]:
-    thread.start()
-for thread in [t1, t2, t3, t4]:
-    thread.join()
-print 'Finished'
-'''
+def zip_files(path, handle):
+    for root, dirs, files in os.walk(path):
+        for f in files:
+            if f.startswith('RDS_') and f.endswith('.txt'): handle.write(os.path.join(path, f), os.path.relpath(os.path.join(root, f), os.path.join(path, '.')))
 
-u1 = Thread(target=unzip_metadata, args=(A, 'A'))
-u2 = Thread(target=unzip_metadata, args=(B, 'B'))
-u3 = Thread(target=unzip_metadata, args=(C, 'C'))
-u4 = Thread(target=unzip_metadata, args=(D, 'D'))
-for thread in [u1, u2, u3, u4]:
-    thread.start()
-for thread in [u1, u2, u3, u4]:
-    thread.join()
+def zip_files_handler():
+    zip_files('RDS_Split/A/', ZipFile('RDS_Split/A/A.zip', 'w', ZIP_DEFLATED))
+    zip_files('RDS_Split/B/', ZipFile('RDS_Split/B/B.zip', 'w', ZIP_DEFLATED))
+    zip_files('RDS_Split/C/', ZipFile('RDS_Split/C/C.zip', 'w', ZIP_DEFLATED))
+    zip_files('RDS_Split/D/', ZipFile('RDS_Split/D/D.zip', 'w', ZIP_DEFLATED))
+    
+def split_threaded():
+    t1 = Thread(target=split_files, args=('RDS_Split/A/RDS_253_A.zip', 'RDS_Split/A/'))
+    t2 = Thread(target=split_files, args=('RDS_Split/B/RDS_253_B.zip', 'RDS_Split/B/'))
+    t3 = Thread(target=split_files, args=('RDS_Split/C/RDS_253_C.zip', 'RDS_Split/C/'))
+    t4 = Thread(target=split_files, args=('RDS_Split/D/RDS_253_D.zip', 'RDS_Split/D/'))
+    for thread in [t1, t2, t3, t4]: thread.start()
+    for thread in [t1, t2, t3, t4]: thread.join()
+    print 'Finished'
 
-#parsed = json.loads(metadata)
-with open('metadata.json', 'w') as j:
-    j.write(json.dumps(metadata, indent=4, sort_keys=True))
-print 'Finished'
+def unzip_threaded():
+    u1 = Thread(target=unzip_metadata, args=(A, 'A'))
+    u2 = Thread(target=unzip_metadata, args=(B, 'B'))
+    u3 = Thread(target=unzip_metadata, args=(C, 'C'))
+    u4 = Thread(target=unzip_metadata, args=(D, 'D'))
+    for thread in [u1, u2, u3, u4]: thread.start()
+    for thread in [u1, u2, u3, u4]: thread.join()
+    with open('metadata.json', 'w') as j: j.write(json.dumps(metadata, indent=4, sort_keys=True))
+    print 'Finished'
+
+split_threaded() # Split the given zipped hash file into several files
+unzip_threaded() # Creates the metadata mapping for file names to hash numbers
+zip_files_handler() # Zips all split hash files together
